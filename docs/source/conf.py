48874125463917d7777049842fb2081b4439b75f
William Niu <wniu721@gmail.com>
2022-01-05 00:48:10 -0500

    Update doc.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,9 +36,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_css_files = [
-    'css/styles.css',
-]
+html_style = 'css/styles.css'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
