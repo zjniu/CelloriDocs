5ab4812193dc12f47031a90b92369f256c1a950c
William Niu <wniu721@gmail.com>
2022-01-05 01:28:57 -0500

    Update doc.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,8 +35,9 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
 html_css_files = [
-    'css/styles.css',
+    'styles.css',
 ]
 
 # -- Options for EPUB output
